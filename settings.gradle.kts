7131a188e15020efe5ec4e9708539930f35e26ee
meza <211977+meza@users.noreply.github.com>
2025-12-09 23:01:36 +0000

    chore: switched to stonecutter 0.8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,8 +15,8 @@ pluginManagement {
 }
 
 plugins {
-    id("gg.meza.stonecraft") version "1.8.9"
-    id("dev.kikugie.stonecutter") version "0.7.+"
+    id("gg.meza.stonecraft") version "1.9.0-SNAPSHOT.7"
+    id("dev.kikugie.stonecutter") version "0.8-beta.1"
 }
 
 stonecutter {
